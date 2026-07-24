f31c2ba5b2ae904a6d1ab3d37326433cc09f3b2d
Jihyun Jeon <hyuneejj@naver.com>
2026-07-24 22:38:54 +0900

    Initial Spring Boot project

---
+++ src/main/java/com/jihyun/englishmate/EnglishMateApplication.java
@@ -0,0 +1,13 @@
+package com.jihyun.englishmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnglishMateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnglishMateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jihyun/englishmate/EnglishMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jihyun.englishmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnglishMateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
